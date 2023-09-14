55be9241da539035cfa869558ec3e7b7e724318b
ricardocollado <ricardocolladorothschild@gmail.com>
2023-09-13 22:12:58 -0400

    complete answer section

--- script/main.js
+++ script/main.js
@@ -1,13 +1,25 @@
 const arrowsElements = document.querySelectorAll('.arrow');
 const elementsAsArray = Array.from(arrowsElements);
+
 const answerElements = document.querySelectorAll('.question-container--answear');
 const answeArray = Array.from(answerElements);
+
+const questions = document.querySelectorAll('.question-container--question');
+const questionsArray = Array.from(questions);
 console.log(typeof arrowsElements);
+let lastPress;
 arrowsElements.forEach(function (element){
     element.addEventListener('click',()=>{
         
+            
         const temp = answeArray[elementsAsArray.indexOf(element)];
+        
         temp.classList.toggle('inactive');
+        const tempQ = questionsArray[elementsAsArray.indexOf(element)];
+        tempQ.classList.toggle('boldQuestion');
+       
     });
 });
 console.log(arrowsElements);
+
+function showingAnswers(0);
