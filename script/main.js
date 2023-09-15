ceb04d62aee21d74f6d7b86af7536719cf0e762d
ricardocollado <ricardocolladorothschild@gmail.com>
2023-09-14 20:01:44 -0400

    fix issue with the box moving when opening a question, now we close any other question open, fix throguth js, using a swithc

--- script/main.js
+++ script/main.js
@@ -6,7 +6,15 @@ const answeArray = Array.from(answerElements);
 
 const questions = document.querySelectorAll('.question-container--question');
 const questionsArray = Array.from(questions);
+let elementsAnsObject = [
+    
+    document.querySelector('a1'),
+    document.querySelector('a2'),
+    document.querySelector('a3'),
+    document.querySelector('a4'),
+    document.querySelector('a5')
 
+];
 
 arrowsElements.forEach((element)=>showingAnswers(element));
 
@@ -14,7 +22,7 @@ questions.forEach(function (element){
     element.addEventListener('click',()=>{
         
         elementsAsArray[questionsArray.indexOf(element)].classList.toggle('arrowReverse');
-
+        checkOpen(questionsArray.indexOf(element));
         const temp = answeArray[questionsArray.indexOf(element)];
         temp.classList.toggle('inactive');       
 
@@ -46,13 +54,27 @@ function showingAnswers(element){
 
 /** */
 
-let elementsAnsObject = [
-    
-        ,
-        document.querySelector('a2'),
-        document.querySelector('a3'),
-        document.querySelector('a4'),
-        document.querySelector('a5')
-    
-];
+
+
+function checkOpen(index){
+    let element;
+    switch(index){
+        case 0:
+            if(!answeArray[1].classList.contains('inactive')){
+                answeArray[1].classList.add('inactive');
+            }
+            if(!answeArray[2].classList.contains('inactive')){
+                answeArray[2].classList.add('inactive');
+            }
+            if(!answeArray[3].classList.contains('inactive')){
+                answeArray[3].classList.add('inactive');
+            }
+            if(!answeArray[3].classList.contains('inactive')){
+                answeArray[3].classList.add('inactive');
+            }
+            
+                      
+            break;
+    }
+}
 
